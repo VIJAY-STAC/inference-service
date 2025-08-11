fb1c27020ede5fdb5aee1d885ce1240743a4f11a
Vijay <vijay.thorat@biddano.com>
2025-08-11 17:56:46 +0530

    removed extra comments

--- predictions/views.py
+++ predictions/views.py
@@ -15,22 +15,12 @@ class PredictView(APIView):
         serializer = PredictionJobSerializer(data=request.data)
         if serializer.is_valid():
             job = serializer.save()
-
-            # Send job to RabbitMQ queue
             try:
-                # url = ""  # from CloudAMQP dashboard
-                # params = pika.URLParameters(url)
-                # # Disable certificate verification (development only!)
-                # params.ssl_options = pika.SSLOptions(ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT), 'leopard.lmq.cloudamqp.com')
-                # params.ssl_options.context.check_hostname = False
-                # params.ssl_options.context.verify_mode = ssl.CERT_NONE
                 params = pika.ConnectionParameters(host="rabbitmq")
-
                 connection = pika.BlockingConnection(params)
             except Exception as e:
                 print("exception is : ", e)
                 return Response("connection not establish")
-
             channel = connection.channel()
             channel.queue_declare(queue='prediction_queue', durable=True)
             channel.basic_publish(
@@ -39,9 +29,6 @@ class PredictView(APIView):
                 body=json.dumps({"job_id": str(job.id), "text": job.text})
             )
             connection.close()
-
-            # cache.set("my_key", "Hello Redis!", timeout=60)  # expires in 60 sec
-
             return Response({"job_id": str(job.id)}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -49,8 +36,6 @@ class PredictView(APIView):
 class PredictStatusView(APIView):
     def get(self, request, job_id):
         job = get_object_or_404(PredictionJob, id=job_id)
-        # value = cache.get("my_key")
-        # print(value)  # Hello Redis!
         return Response({
             "status": job.status,
             "result": job.result
--- worker.py
+++ worker.py
@@ -11,27 +11,16 @@ django.setup()
 
 from predictions.models import PredictionJob
 
-print("model code start here ....")
-model_name = "openai-community/gpt2"
-# or use: model_name = "facebook/opt-iml-max-1.3b"
+model_name = "openai-community/gpt2" # or "facebook/opt-iml-max-1.3b"
 token = "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"
-
-# Load tokenizer and model with authentication if token is set
-print("model code start here  ####....")
 tokenizer = AutoTokenizer.from_pretrained(model_name, token=token if token else None)
-print("model code start here  ******")
 model = AutoModelForCausalLM.from_pretrained(model_name, token=token if token else None)
-print("model found ******")
 
-# Move model to GPU if available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
 def generate_answer(prompt: str) -> str:
-    # Tokenize input and move tensors to model device
     inputs = tokenizer(prompt, return_tensors="pt").to(device)
-
-    # Generate output tokens with sampling and temperature
     outputs = model.generate(
         **inputs,
         max_new_tokens=200,
@@ -42,15 +31,10 @@ def generate_answer(prompt: str) -> str:
     )
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-# ---- RabbitMQ callback ----
 def callback(ch, method, properties, body):
-    print("task picked")
     data = json.loads(body)
     job_id = data.get("job_id")
-    text = data.get("text")
-
-    jobs = PredictionJob.objects.all().count()
-    
+    text = data.get("text")    
     try:
         job = PredictionJob.objects.get(id=job_id)
     except PredictionJob.DoesNotExist:
@@ -62,30 +46,17 @@ def callback(ch, method, properties, body):
         result = generate_answer(text)
         job.status = "done"
         job.result = result
-        print("result found.")
     except Exception as e:
         job.status = "failed"
         job.result = str(e)
-        print("result not found.")
 
     job.save()
     ch.basic_ack(delivery_tag=method.delivery_tag)
         
-
-
-# url = ""  
-# params = pika.URLParameters(url)
-# # Disable certificate verification (development only!)
-# params.ssl_options = pika.SSLOptions(ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT), 'leopard.lmq.cloudamqp.com')
-# params.ssl_options.context.check_hostname = False
-# params.ssl_options.context.verify_mode = ssl.CERT_NONE
-
-
 params = pika.ConnectionParameters(host="rabbitmq")
 connection = pika.BlockingConnection(params)
 channel = connection.channel()
 channel.queue_declare(queue='prediction_queue', durable=True)
-# channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue='prediction_queue', on_message_callback=callback, auto_ack=False)
 print("Consumer ready to consume Rabbitmq")
 channel.start_consuming()
